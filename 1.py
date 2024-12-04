94eb48ebdc748c491c82d4acbb3d8f8325226734
Vladislav Efremov <vlad.efrrvov@mail.ru>
2024-12-05 02:21:37 +0300

    init

--- 1.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-gpus = tf.config.list_physical_devices()
-if gpus:
-    print("Num GPUs Available: ", len(gpus))
-    print("Available GPUs: ", gpus[0].name)
-else:
-    print("GPU не найден")
